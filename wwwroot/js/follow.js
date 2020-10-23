4e8a7d821545aafeac77545931edd3d3d8ad88e1
Mike Wild <wildconsultingltd@googlemail.com>
2020-10-23 12:04:32 +0100

    Various udates

---
+++ wwwroot/js/follow.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $("#new-follow").click(function () {
+
+        //toggle menu on click
+        if ($("#follow_menu").hasClass("tl-none")) {
+            $("#follow_menu").removeClass("tl-none");
+        } else {
+            $("#follow_menu").addClass("tl-none");
+        }
+
+        //Show tick if user is subscribed
+        //TODO: Add "if"
+        $("#selection-tick").removeClass("tl-none");
+        
+        return false;
+    });
+});
